2f0069a3d9996e6d617c610b29a2677bfe05b7d3
Sledger <beefy1875@hotmail.co.uk>
2022-11-24 16:15:28 +0700

    ex1.2 course info step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,17 +9,18 @@ const Header = ({course}) => {
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-        <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
     </div>
   )
+}
+
+const Part = ({part, exercise}) => {
+  return (
+    <p>{part} {exercise}</p>
+  )
+
 }
 
 const Total = ({total}) => {
